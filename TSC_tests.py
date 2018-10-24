00ba4b53fecd6137c9547f3f523305abc0a04d89
Maxwell Hunter <maxwell@treehousestickers.com>
2018-10-24 13:55:31 -0700

    Updated TSC: tests, pep8, accuracy imrovements
    -added new unittests
    -added re to import
    -made pep8 improvements
    -inititalizing tw_api with twitterSetup()
    -updated get_syllables_phrase() function
    -added sanitize() function
    -updated syllable_count(), and ignore() to first sanitize words

--- TSC_tests.py
+++ TSC_tests.py
@@ -1,45 +1,73 @@
 import unittest
-
-import tweepy
 import tweet_syllable_count
 
+
 class TestFunctions(unittest.TestCase):
-	def test_WordCount(self):
-		#test Word Count function
-		self.assertEqual(tweet_syllable_count.word_count('K'), 1)
-		self.assertEqual(tweet_syllable_count.word_count('Hello World'), 2)
-		self.assertEqual(tweet_syllable_count.word_count('The Ball is Red'), 4)
+    def test_WordCount(self):
+        # test Word Count function
+        self.assertEqual(tweet_syllable_count.word_count('K'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('Hello World'), 2)
+        self.assertEqual(tweet_syllable_count.word_count('The Ball is Red'), 4)
+        self.assertEqual(tweet_syllable_count.word_count(':'), 0)
+        self.assertEqual(tweet_syllable_count.word_count('a'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('k'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('Be'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('bad'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('sad'), 1)
+        self.assertEqual(tweet_syllable_count.word_count("they're"), 1)
+        self.assertEqual(tweet_syllable_count.word_count('GOP'), 0)
+        self.assertEqual(tweet_syllable_count.word_count('#MAGA'), 0)
+        self.assertEqual(tweet_syllable_count.word_count('bounce'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('conscious'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('"Mother'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('squishy'), 1)
+        self.assertEqual(tweet_syllable_count.word_count('American'), 1)
+        self.assertEqual(tweet_syllable_count.word_count(
+            'super-volcano'), 1)
 
-	def test_SyllableCount(self):
-		#test syllable counter function
-		self.assertEqual(tweet_syllable_count.syllable_count('a'),1)
-		self.assertEqual(tweet_syllable_count.syllable_count(' '),1)
-		self.assertEqual(tweet_syllable_count.syllable_count('k'),1)
-		self.assertEqual(tweet_syllable_count.syllable_count('Be'),1)
-		self.assertEqual(tweet_syllable_count.syllable_count('bounce'),1)
-		self.assertEqual(tweet_syllable_count.syllable_count('conscious'),2)
+    def test_SyllableCount(self):
+        # test syllable counter function
+        self.assertEqual(tweet_syllable_count.syllable_count('K'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('Hello World'), 3)
+        self.assertEqual(tweet_syllable_count.syllable_count('The Ball is Red'), 4)
+        self.assertEqual(tweet_syllable_count.syllable_count(' '), 0)
+        self.assertEqual(tweet_syllable_count.syllable_count(':'), 0)
+        self.assertEqual(tweet_syllable_count.syllable_count('a'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('k'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('Be'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('bad'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('sad'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count("they're"), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('GOP'), 0)
+        self.assertEqual(tweet_syllable_count.syllable_count('#MAGA'), 0)
+        self.assertEqual(tweet_syllable_count.syllable_count('bounce'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('conscious'), 2)
+        self.assertEqual(tweet_syllable_count.syllable_count('"Mother'), 2)
+        self.assertEqual(tweet_syllable_count.syllable_count('squishy'), 2)
+        self.assertEqual(tweet_syllable_count.syllable_count('American'), 4)
+        self.assertEqual(tweet_syllable_count.syllable_count(
+            'super-volcano'), 5)
 
+    def test_Gettysburg(self):
+        self.assertEqual(tweet_syllable_count.syllable_count('four'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('score'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('and'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('seven'), 2)
+        self.assertEqual(tweet_syllable_count.syllable_count('years'), 1)
+        self.assertEqual(tweet_syllable_count.syllable_count('ago'), 2)
 
-	def test_Gettysburg(self):
-		self.assertEqual(tweet_syllable_count.syllable_count('four'),1)
-		self.assertEqual(tweet_syllable_count.syllable_count('score'),1)
-		self.assertEqual(tweet_syllable_count.syllable_count('and'),1)
-		self.assertEqual(tweet_syllable_count.syllable_count('seven'),2)
-		self.assertEqual(tweet_syllable_count.syllable_count('years'),1)
-		self.assertEqual(tweet_syllable_count.syllable_count('ago'),2)
+    def test_Syllables_phrase(self):
+        self.assertEqual(tweet_syllable_count.get_syllables_phrase(
+            'four score, seven years ago'), 7)
 
-	def test_Syllables_phrase(self):
-		self.assertEqual(tweet_syllable_count.get_syllables_phrase('four score and seven years ago'),8)
 
 class TestLiveFunctions(unittest.TestCase):
-    def test_keys(self):
-        self.assertEqual(tweet_syllable_count.initTwitter(), 1)
-        
     def test_cloudhouseTweet(self):
         self.assertEqual(
-            tweet_syllable_count.get_syllables_per_word(
-                'tehclubhouse'),
-                'This tweet from @tehclubhouse has a 1.11 syllable to word ratio. \n\t\thttps://twitter.com/15387997/status/530408516641886209'
-            )
+            tweet_syllable_count.get_syllables_per_word('tehclubhouse').split(
+                    'syllable')[0],
+                'This tweet from @tehclubhouse has a 1.00 ')
+
+
 if __name__ == '__main__':
-	unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
--- tweet_syllable_count.py
+++ tweet_syllable_count.py
@@ -1,129 +1,175 @@
 # -*- coding: utf-8 -*-
 # imports
 import datetime
+import re
 import time
 import tweepy
-from tw_keys import *
+
+import tw_keys
 
 # variables setups
 # inaert twitter API keys here, or use tw_keys
 '''
-TW_CONSUMER_KEY=	''
-TW_CONSUMER_SECRET=	''
-TW_ACCESS_TOKEN=	''
-TW_ACCESS_SECRET=	''
+TW_CONSUMER_KEY=    ''
+TW_CONSUMER_SECRET=    ''
+TW_ACCESS_TOKEN=    ''
+TW_ACCESS_SECRET=    ''
 '''
 
-#twitter API setup
-def twitterSetup():
-	tw_auth=tweepy.OAuthHandler(TW_CONSUMER_KEY, TW_CONSUMER_SECRET)
-	tw_auth.set_access_token(TW_ACCESS_TOKEN, TW_ACCESS_SECRET)
-	tw_api=tweepy.API(tw_auth)
-	return tw_api
 
-tw_api = twitterSetup()
+# twitter API setup
+def twitterSetup():
+    tw_auth = tweepy.OAuthHandler(
+        tw_keys.TW_CONSUMER_KEY, tw_keys.TW_CONSUMER_SECRET)
+    tw_auth.set_access_token(
+        tw_keys.TW_ACCESS_TOKEN, tw_keys.TW_ACCESS_SECRET)
+    tw_api = tweepy.API(tw_auth)
+    return tw_api
+
 
 # functions
 def get_syllables_phrase(phrase):
-	#split phrase into words
-	words = phrase.split(' ')
-	#init counter
-	syllables = 0
-	for word in words:
-		syllables += syllable_count(word)
-	return syllables
+    #split phrase into words
+    words = phrase.split(' ')
+    # init counter
+    syllables = 0
+    for word in words:
+        syllables += syllable_count(word)
+    return syllables
+
+
+def sanitize(word):
+    return re.sub("""[-'".,!?]""", '', word)
+
 
 def ignore(word):
-	#if true, the word is ignored and not counted
-	#ignore words that start with @ and #
-	if word[0] in '@#':
-		return True
-	#ignore words in all Caps that are more than 1, and less than 5 letters long
-	if len(word) < 5 and len(word) > 1 and (word == word.upper()):
-		return True
-	if not word.isalpha():
-		return True
-	else:
-		return False
+    # if true, the word is ignored and not counted
+    # ignore words that start with @ and #
+    if word[0] in '@#':
+        return True
+    # ignore words in all Caps that are more than 1, and less than 5 letters
+    if len(word) < 5 and len(word) > 1 and (word == word.upper()):
+        return True
+    # remove all characters that are allowed in some words (like hyphens)
+    word = sanitize(word)
+    if not word.isalpha():
+        if ''.join(list(word).pop(-1)).isalpha():
+            return False
+        else:
+            return True
+    else:
+        return False
 
-def syllable_count(word): #count the number of syllables per word
-	if ignore(word):
-		return 0
-	word = word.lower() #lowercase everthing
-	syllable_count = 0 #track syllable count
-	vowels = "aeiouy" # we're using vowels to detirmine syllables
-	if word[0] in vowels: #if the word starts with a vowel
-		syllable_count += 1 #increment syllable_count
-	for index in range(1, len(word)): #for the rest of the word
-		#if the letter is a vowel, and the letter before it isn't,
-		if (word[index] in vowels) and (word[index - 1] not in vowels):
-			syllable_count += 1 #increment syllable_count
-			if word.endswith("e"): #if the word ends with "e"
-				syllable_count -= 1 #deincrement syllable_count
-	if syllable_count == 0: #if it's a word that doesn't have a syllable yet,
-		syllable_count += 1 #increment syllable_count
-	return syllable_count #return the count
 
-def word_count(phrase): #count the number of words
-	#return the number of words separated by whitespace
-	counter = 0
-	# split phrase into separate words
-	words = phrase.split(' ')
-	for word in words:
-		#only count the word if it's on the ignore list
-		if not ignore(word):
-			counter += 1
-	#return the number of words separated by whitespace
-	return counter
+# count the number of syllables per word
+def syllable_count(word):
+    if ignore(word):
+        return 0
+    # lowercase everthing
+    word = word.lower()
+    # remove all characters that are allowed in some words (like hyphens)
+    word = sanitize(word)
+    # track syllable count
+    syllable_count = 0
+    # we're using vowels to detirmine syllables
+    vowels = "aeiouy"
+    # if the word starts with a vowel
+    if word[0] in vowels:
+        # increment syllable_count
+        syllable_count += 1
+    # for the rest of the word
+    for index in range(1, len(word)):
+        # if the letter is a vowel, and the letter before it isn't,
+        if (word[index] in vowels) and (word[index - 1] not in vowels):
+            # increment syllable_count
+            syllable_count += 1
+            # if the word ends with "e"
+            if word.endswith("e"):
+                #deincrement syllable_count
+                syllable_count -= 1
+    # if it's a word that doesn't have a syllable yet,
+    if syllable_count == 0:
+        # increment syllable_count
+        syllable_count += 1
+    # return the count
+    return syllable_count
 
-#get the syllables to word ratio
+
+# count the number of words
+def word_count(phrase):
+    # return the number of words separated by whitespace
+    counter = 0
+    # split phrase into separate words
+    words = phrase.split(' ')
+    for word in words:
+        # only count the word if it's on the ignore list
+        if not ignore(word):
+            counter += 1
+    # return the number of words separated by whitespace
+    return counter
+
+
+# get the syllables to word ratio
 def get_syllables_per_word(at_user):
-	#get the selected user with the api
-	user = tw_api.get_user(at_user)
-	#get the most recent tweet
-	status=user.timeline()[0]
-	#get text the the tweet
-	tweet = status.text.encode('ascii', 'ignore')
+    # get the selected user with the api
+    user = tw_api.get_user(at_user)
+    # get the most recent tweet
+    status = user.timeline()[0]
+    # get text the the tweet
+    tweet = status.text.encode('ascii', 'ignore')
 
-	#get syllable count
-	s_count=get_syllables_phrase(tweet)
-	#get word count
-	w_count=word_count(str(tweet))
-	#get the ratio
-	ratio = (float(s_count)/float(w_count))
+    # get syllable count
+    s_count = get_syllables_phrase(tweet)
+    # get word count
+    w_count = word_count(str(tweet))
+    # get the ratio
+    ratio = (float(s_count)/float(w_count))
 
-	#return the formatted text
-	return """This tweet from @{} has a {} syllable to word ratio. 
-		https://twitter.com/{}/status/{}""".format(
-			at_user,
-			"{0:.2f}".format(float(s_count)/float(w_count)),
-			user.id,
-			status.id
-			)
-#change this to target user
-tracked_users = ['neiltyson','realdonaldtrump']
+    # return the formatted text
+    return """This tweet from @{} has a {} syllable to word ratio. 
+        https://twitter.com/{}/status/{}""".format(
+            at_user,
+            "{0:.2f}".format(ratio),
+            user.id,
+            status.id
+            )
+
+
+# change this to target user
+tracked_users = ['neiltyson', 'realdonaldtrump']
 print 'boot sequence complete'
 
-#loop forever - check for new tweets, then wait 10 minutes
+
+tw_api = twitterSetup()
+
+
+# loop forever - check for new tweets, then wait 10 minutes
 if __name__ == '__main__':
-	#print initializtion sequence
-	while True:
-		for user in tracked_users:
-			print(
-				"Checking for tweets from @{}".format(user))
-			try:
-				#generate ratio tweet text
-				new_tweet = get_syllables_per_word(user) 
-				#send the tweet - will throw exception if it's already been sent
-				tw_api.update_status(new_tweet) 
-				#print a sucess message
-				print("Sending the following tweet:")
-				print(new_tweet)
-			#non unique text error
-			except tweepy.error.TweepError: 
-				#print failure message
-				print("no new tweets, not posting right now.") 
-			#print wait message
-			print "{} - sleeping for ten minutes".format(datetime.datetime.now())
-		#wait ten minutes
-		time.sleep(600) 
+    # print initializtion sequence
+    while True:
+        for user in tracked_users:
+            print("Checking for tweets from @{}".format(user))
+            # generate ratio tweet text
+            newTweet = get_syllables_per_word(user)
+            # send the tweet if it's not in the timeline
+            duplicate = False
+            myTwitter = tw_api.get_user('SyllableCounter')
+            for myTweet in myTwitter.timeline():
+                # if the new tweet has the same ratio as another recent tweet
+                if (newTweet.split('https')[0]) == (
+                   myTweet.text.split('https')[0]):
+                    # tweet already sent
+                    print 'Tweet already sent'
+                    # mark it as a duplicate
+                    duplicate = True
+            # if it's not marked as a duplicate
+            if not duplicate:
+                # send the tweet
+                tw_api.update_status(newTweet) 
+                # print a sucess message
+                print("Sending the following tweet:")
+                print(newTweet)
+        # print wait message
+        print "{} - sleeping for ten minutes".format(datetime.datetime.now())
+        # wait ten minutes
+        time.sleep(600) 
